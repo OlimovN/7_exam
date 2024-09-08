575c7f13753d645761055cd9d22402d5bb831132
OlimovN <olimonurmuhammad711@gmail.com>
2024-09-08 07:30:29 +0500

    add mode

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,22 @@
+// src/pages/HomePage.js
 import React, { useEffect, useState, useMemo, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import http from "../utils/axios.js";
 import Loader from "../components/Loader.jsx";
+import { useTheme } from "../context/ThemeContext"; // Import the theme context
 
 const HomePage = () => {
   const [card, setCard] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [region, setRegion] = useState("");
   const navigate = useNavigate();
+  const { isDarkMode } = useTheme(); // Use the theme context
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await http.get(`/countries`);
-        console.log(response);
         if (response && response.data && response.data.data) {
           setCard(response.data.data);
         } else {
@@ -47,7 +49,9 @@ const HomePage = () => {
   );
 
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div
+      className={`min-h-screen ${isDarkMode ? "bg-gray-900" : "bg-gray-100"}`}
+    >
       <Header setSearchQuery={setSearchQuery} setRegion={setRegion} />
       <div className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
@@ -55,7 +59,9 @@ const HomePage = () => {
             filteredCards.map((crds, index) => (
               <div
                 onClick={() => handleCardClick(crds.name.slug)}
-                className="bg-white rounded-lg shadow-md overflow-hidden cursor-pointer hover:shadow-lg transition-shadow duration-300"
+                className={`${
+                  isDarkMode ? "bg-gray-800" : "bg-white"
+                } rounded-lg shadow-md overflow-hidden cursor-pointer hover:shadow-lg transition-shadow duration-300`}
                 key={index}
               >
                 <img
@@ -64,16 +70,32 @@ const HomePage = () => {
                   className="w-full h-48 object-cover"
                 />
                 <div className="p-4">
-                  <h2 className="text-xl font-semibold mb-2">
+                  <h2
+                    className={`text-xl font-semibold mb-2 ${
+                      isDarkMode ? "text-white" : "text-gray-900"
+                    }`}
+                  >
                     {crds.name.common}
                   </h2>
-                  <p className="text-gray-700 mb-1">
+                  <p
+                    className={`text-gray-700 ${
+                      isDarkMode ? "text-gray-300" : ""
+                    }`}
+                  >
                     <strong>Population:</strong> <span>{crds.population}</span>
                   </p>
-                  <p className="text-gray-700 mb-1">
+                  <p
+                    className={`text-gray-700 ${
+                      isDarkMode ? "text-gray-300" : ""
+                    }`}
+                  >
                     <strong>Region:</strong> <span>{crds.region}</span>
                   </p>
-                  <p className="text-gray-700">
+                  <p
+                    className={`text-gray-700 ${
+                      isDarkMode ? "text-gray-300" : ""
+                    }`}
+                  >
                     <strong>Capital:</strong>{" "}
                     <span>{crds.capital && crds.capital[0]}</span>
                   </p>
@@ -82,7 +104,7 @@ const HomePage = () => {
             ))
           ) : (
             <div className="flex justify-center items-center col-span-full h-48">
-            <Loader></Loader>
+              <Loader />
             </div>
           )}
         </div>
